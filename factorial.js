0b97a6c45e953c11f972e3a407cd1018fdce8d16
nordev <aa.nortes@gmail.com>
2018-01-04 14:23:55 +0100

    Solution for factorial in js

---
+++ factorial.js
@@ -0,0 +1,11 @@
+/*
+Using the JavaScript language, have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it (e.g. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18 and the input will always be an integer. 
+*/
+
+function FirstFactorial(num) { 
+  sol = 1
+  for (index = num; index > 0; --index) 
+    sol = sol * index 
+  return sol; 
+}
+FirstFactorial(readline());
